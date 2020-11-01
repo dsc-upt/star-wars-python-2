d5e9196134bff0ef2793ab3290f93107fc2dabb1
Gaita <alexandru.gaita@student.upt.ro>
2020-11-01 21:05:34 +0200

    scrapper

--- Alex Gaita/email_sender.py
+++ Alex Gaita/email_sender.py
@@ -1,9 +1,18 @@
-from getpass import getpass
+from getpass import  getpass
+import  yagmail
+
 
 def main():
     username = "alex.gaita99@gmail.com"
-    password = getpass()
+    password=getpass()
+    mail_client=yagmail.SMTP(username, password)
+    receiver = input("Insert email: ")
+    if "@" not in receiver:
+        print("Email invalid")
+    else:
+        mail_client.send(receiver, "Test","body")
+        print("Mail sent")
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
