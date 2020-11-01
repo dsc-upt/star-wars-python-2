52d064df3b98d2a55815224602646d8d71468e8a
Cristina Popescu <mihaelacristina.pope@student.upt.ro>
2020-11-01 21:04:59 +0200

    added twitter scraper

---
+++ Cristina Popescu/twitter_scraper.py
@@ -0,0 +1,16 @@
+import json
+import urllib.request
+
+TWITTER_API = "https://localhost-python-abstraction.glitch.me"
+
+
+def fetch(username):
+    url = TWITTER_API + "/api/tweets/" + username
+    request = urllib.request.Request(url)
+    response = urllib.request.urlopen(request).read()
+    decoded_response = response.decode('utf-8')
+    data = json.loads(decoded_response)
+    return data['data']
+
+
+print(fetch('elonmusk'))
